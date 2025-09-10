7e2209465c5634292e11091fde1cb71b27e3528a
AufanT <aufantaufiq08905@gmail.com>
2025-09-10 21:23:16 +0700

    fixixixi

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path'); // Tambahkan baris ini
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -13,10 +13,12 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-app.get('/', (req, res) => { res.send('Welcome to API EcoPick'); });
+app.get('/', (req, res) => { 
+    res.send({ message: 'Welcome to the EcoPick API!' });
+});
 
-// Perbaikan: Gunakan path.join untuk menemukan file YAML
-const swaggerDocument = YAML.load(path.join(process.cwd(), 'dokumentasi-api.yaml'));
+// Import routes
+const swaggerDocument = YAML.load(path.join(__dirname, 'dokumentasi-api.yaml'));
 const authRoutes = require('./routes/Auth.routes');
 const userRoutes = require('./routes/User.routes');
 const adminRoutes = require('./routes/Admin.routes');
@@ -26,9 +28,11 @@ const orderRoutes = require('./routes/Order.routes.js');
 const wishlistRoutes = require('./routes/Wishlist.routes.js');
 const trackingRoutes = require('./routes/Tracking.routes.js');
 
+// Setup Swagger UI
 app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
+// Setup routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/admin', [verifyToken, getUserWithRole, isAdmin], adminRoutes);
@@ -38,6 +42,8 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/wishlist', wishlistRoutes);
 app.use('/api/tracking', trackingRoutes);
 
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`🚀 Server is running on port ${PORT}`);
+    console.log(`📚 API Documentation: http://localhost:${PORT}/api-docs`);
 });
